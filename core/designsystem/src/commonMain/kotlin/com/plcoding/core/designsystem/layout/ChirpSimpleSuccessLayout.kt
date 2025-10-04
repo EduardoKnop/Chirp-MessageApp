0a9f0cbdaa6dc71a4c1a55ca672a811d27e6e94a
Eduardo Knop <eduk.coelho@hotmail.com>
2025-10-03 21:10:25 -0300

    Add registration success screen and view model with email verification actions

--- core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/layout/ChirpSimpleSuccessLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/layout/ChirpSimpleSuccessLayout.kt
@@ -50,7 +50,7 @@ fun ChirpSimpleSuccessLayout(
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = description,
-                style = MaterialTheme.typography.titleLarge,
+                style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.extended.textSecondary,
                 textAlign = TextAlign.Center,
             )

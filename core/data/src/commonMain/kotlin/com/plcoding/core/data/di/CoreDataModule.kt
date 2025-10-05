628fe3a9edd74cc6466aa2d749ae4cfa21704132
Eduardo Knop <eduk.coelho@hotmail.com>
2025-10-05 01:16:03 -0300

    Add splash screen support and update app theme

--- core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataModule.kt
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataModule.kt
@@ -17,7 +17,7 @@ expect val platformCoreDataModule: Module
 val coreDataModule = module {
     includes(platformCoreDataModule)
     single<ChirpLogger> { KermitLogger }
-    single { HttpClientFactory(get()).create(get()) }
+    single { HttpClientFactory(get(), get()).create(get()) }
     singleOf(::KtorAuthService) bind AuthService::class
     singleOf(::DataStoreSessionStorage) bind SessionStorage::class
 }

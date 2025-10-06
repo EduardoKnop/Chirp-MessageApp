2daaca6d13e14fed3484370e07f8bd97058b412c
Eduardo Knop <eduk.coelho@hotmail.com>
2025-10-05 21:15:30 -0300

    Add Forgot Password feature implementation

--- feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/forgot_password/ForgotPasswordViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/forgot_password/ForgotPasswordViewModel.kt
@@ -1,21 +1,34 @@
 package com.plcoding.auth.presentation.forgot_password
 
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.plcoding.auth.domain.EmailValidator
+import com.plcoding.core.domain.auth.AuthService
+import com.plcoding.core.domain.util.onFailure
+import com.plcoding.core.domain.util.onSuccess
+import com.plcoding.core.presentation.util.toUiText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class ForgotPasswordViewModel : ViewModel() {
+class ForgotPasswordViewModel(
+    private val authService: AuthService,
+) : ViewModel() {
     
     private var hasLoadedInitialData = false
-    
     private val _state = MutableStateFlow(ForgotPasswordState())
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
-                /** Load initial data here **/
+                observeValidationState()
                 hasLoadedInitialData = true
             }
         }
@@ -24,10 +37,59 @@ class ForgotPasswordViewModel : ViewModel() {
             started = SharingStarted.WhileSubscribed(5_000L),
             initialValue = ForgotPasswordState(),
         )
+    private val isEmailValidFlow = snapshotFlow { state.value.emailTextFieldState.text.toString() }
+        .map { email -> EmailValidator.validate(email) }
+        .distinctUntilChanged()
+    
+    private fun observeValidationState() {
+        isEmailValidFlow.onEach { isValid ->
+            _state.update {
+                it.copy(
+                    canSubmit = isValid,
+                )
+            }
+        }.launchIn(viewModelScope)
+    }
     
     fun onAction(action: ForgotPasswordAction) {
         when (action) {
-            else -> null //TODO: Replace with Events
+            ForgotPasswordAction.OnSubmitClick -> submitForgotPassword()
+        }
+    }
+    
+    private fun submitForgotPassword() {
+        if (state.value.isLoading || !state.value.canSubmit) {
+            return
+        }
+        
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoading = true,
+                    isEmailSentSuccessfully = false,
+                    errorText = null,
+                )
+            }
+            
+            val email = _state.value.emailTextFieldState.text.toString()
+            authService
+                .forgotPassword(email)
+                .onSuccess {
+                    _state.update {
+                        it.copy(
+                            isEmailSentSuccessfully = true,
+                            isLoading = false,
+                        )
+                    }
+                }
+                .onFailure { error ->
+                    _state.update {
+                        it.copy(
+                            errorText = error.toUiText(),
+                            isLoading = false,
+                        )
+                    }
+                }
         }
     }
     

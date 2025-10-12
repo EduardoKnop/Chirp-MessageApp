4082dddd9732c046273fbdb3deb8b00758896e96
Eduardo Knop <114895260+EduardoKnop@users.noreply.github.com>
2025-10-11 23:26:56 -0300

    Add chat creation functionality with Ktor integration and update related models

--- feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/components/ManageChatButtonSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/components/ManageChatButtonSection.kt
@@ -1,11 +1,19 @@
 package com.plcoding.chat.presentation.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -13,14 +21,32 @@ fun ManageChatButtonSection(
     primaryButton: @Composable () -> Unit,
     secondaryButton: @Composable () -> Unit,
     modifier: Modifier = Modifier,
+    error: String? = null,
 ) {
-    Row(
-        modifier = modifier
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End),
+    Column(
+        modifier = modifier.padding(16.dp),
     ) {
-        secondaryButton()
-        primaryButton()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End),
+        ) {
+            secondaryButton()
+            primaryButton()
+        }
+        AnimatedVisibility(
+            visible = error != null
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            error?.let {
+                Text(
+                    text = it,
+                    textAlign = TextAlign.End,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelSmall,
+                    modifier = Modifier.align(Alignment.End),
+                )
+            }
+        }
     }
 }

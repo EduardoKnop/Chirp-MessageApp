de8cd1027a55488d29436c356506a3acaf02cacd
Eduardo Knop <114895260+EduardoKnop@users.noreply.github.com>
2025-10-28 20:22:49 -0300

    Add chat participant repository and service implementations

--- feature/chat/data/src/commonMain/kotlin/com/plcoding/chat/data/di/ChatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/com/plcoding/chat/data/di/ChatDataModule.kt
@@ -1,7 +1,6 @@
 package com.plcoding.chat.data.di
 
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import com.plcoding.chat.data.chat.KtorChatParticipantService
 import com.plcoding.chat.data.chat.KtorChatService
 import com.plcoding.chat.data.chat.OfflineFirstChatRepository
 import com.plcoding.chat.data.chat.WebSocketChatConnectionClient
@@ -10,18 +9,20 @@ import com.plcoding.chat.data.message.KtorChatMessageService
 import com.plcoding.chat.data.message.OfflineFirstMessageRepository
 import com.plcoding.chat.data.network.ConnectionRetryHandler
 import com.plcoding.chat.data.network.KtorWebSocketConnector
+import com.plcoding.chat.data.participant.KtorChatParticipantService
+import com.plcoding.chat.data.participant.OfflineFirstChatParticipantRepository
 import com.plcoding.chat.database.DatabaseFactory
 import com.plcoding.chat.domain.chat.ChatConnectionClient
-import com.plcoding.chat.domain.chat.ChatParticipantService
 import com.plcoding.chat.domain.chat.ChatRepository
 import com.plcoding.chat.domain.chat.ChatService
 import com.plcoding.chat.domain.message.MessageRepository
+import com.plcoding.chat.domain.participant.ChatParticipantRepository
+import com.plcoding.chat.domain.participant.ChatParticipantService
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
-
 
 expect val platformChatDataModule: Module
 
@@ -36,6 +37,7 @@ val chatDataModule = module {
     singleOf(::ConnectionRetryHandler)
     singleOf(::KtorWebSocketConnector)
     singleOf(::KtorChatMessageService) bind ChatMessageService::class
+    singleOf(::OfflineFirstChatParticipantRepository) bind ChatParticipantRepository::class
     single {
         Json {
             ignoreUnknownKeys = true
